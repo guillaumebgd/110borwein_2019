b804f484a5634e4e041d77ca2d5ac114cc952da9
gbogard <guillaume.bogard-coquard@epitech.eu>
2020-04-14 00:26:35 +0200

    Update: 110borwein finished

--- midpoint_method.py
+++ midpoint_method.py
@@ -9,6 +9,16 @@
 from borwein_func_to_integrate import borwein_func_to_integrate
 
 def midpoint_method(n: int):
+    a = 0
+    b = 5000
+    sub_intervals = 10000
+    sub_interval_length = (b - a) / sub_intervals
     In = 0.0
 
+    i = 0
+    x = sub_interval_length / 2
+    while i <= sub_intervals - 1:
+        In += sub_interval_length * borwein_func_to_integrate(n, x)
+        x += sub_interval_length
+        i += 1
     return In
